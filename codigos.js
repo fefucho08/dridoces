4cc368509cc4c5fa80f5775c8c33a4bd5162d7cd
fefucho08 <63268454+fefucho08@users.noreply.github.com>
2020-04-11 18:40:48 -0300

    Add files via upload

---
+++ codigos.js
@@ -0,0 +1,6 @@
+function instagram(){
+  window.location.href = "https://www.instagram.com/adridoces.fca/"
+}
+function facebook(){
+  window.location.href= "https://www.facebook.com/adridocescaseiros/"
+}
